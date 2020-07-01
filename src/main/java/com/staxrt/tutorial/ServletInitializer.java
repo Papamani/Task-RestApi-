c6b410df2ea878cb738854ea5f9f4a2d75f8e0cc
Papamani <30549690+Papamani@users.noreply.github.com>
2020-07-01 13:11:06 +0530

    Create ServletInitializer.java

---
+++ src/main/java/com/staxrt/tutorial/ServletInitializer.java
@@ -0,0 +1,23 @@
+package com.staxrt.tutorial;
+
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+
+
+	@Override
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(SpringAppApplication.class);
+
+	}
+
+}
